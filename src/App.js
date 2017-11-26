74623321c17e62a8aa7cea20a7f4fb0ae5392d77
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-26 03:29:04 -0500

    added second container

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import './App.css';
 //import Components
 import Layout from './hoc/Layout/Layout';
 import TeamPicker from './containers/TeamPicker/TeamPicker';
-
+import YourTeam from './containers/YourTeam/YourTeam';
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <Switch>
+          <Route path="/yourteam" component={YourTeam} />
           <Route path="/" exact component={TeamPicker} />
         </Switch>
       </Layout>
